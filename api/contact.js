247534345250c21b57aa6f9ee84bfdab9eb4b81e
didiso7016 <127390229+didiso7016@users.noreply.github.com>
2026-05-29 23:12:29 +0800

    那掉from 的 send

--- api/contact.js
+++ api/contact.js
@@ -35,7 +35,7 @@ export default async function handler(req, res){
 
         const data = await resend.emails.send({
 
-            from:'EnviroValor Website <noreply@send.envirovalor.com>',
+            from:'EnviroValor Website <noreply@envirovalor.com>',
 
             to:'info@envirovalor.com',
 
